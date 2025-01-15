7ca430250bf69fb967df849486a42f998858ef21
Lei Li <lileicc@gmail.com>
2025-01-15 12:13:44 -0500

    small fix to example vector add

--- simple_cuda_demo/example_vector_add.cu
+++ simple_cuda_demo/example_vector_add.cu
@@ -5,6 +5,9 @@
 #include <fstream>
 
 __global__ void VecAddKernel(int* A, int* B, int* C, int n) {
+  // blockDim is size of block along x-axis
+  // blockIdx is the index of the current thread's block
+  // threadIdx is the index of the current thread within the block
   // Compute the global thread ID
   int i = blockDim.x * blockIdx.x + threadIdx.x;
   if (i < n) {
@@ -53,4 +56,43 @@ void VecAddCUDA2(int* Acpu, int* Bcpu, int* Ccpu, int n) {
   cudaFree(dC);
 }
 
+}
+
+// Check result on the CPU
+void verify_result_add(vector<int> &a, vector<int> &b, vector<int> &c, int N) {
+  // For every row...
+  for (int i = 0; i < N; i++) {
+    // For every column...
+    for (int j = 0; j < N; j++) {
+      // For every element in the row-column pair
+      // Check against the CPU result
+      if (a[i * N + j] + b[i * N + j] != c[i * N + j]) {
+        printf("Error in (%d, %d): %d + %d != %d\n", i, j, a[i * N + j], b[i * N + j], c[i * N + j]);
+      }
+      assert(a[i * N + j] + b[i * N + j] == c[i * N + j]);
+    }
+  }
+}
+
+int main() {
+  // length of the vector
+  int n = 1024;
+
+  // Host vectors
+  vector<int> h_a(N * N);
+  vector<int> h_b(N * N);
+  vector<int> h_c(N * N);
+
+  // Initialize matrices
+  generate(h_a.begin(), h_a.end(), []() { return rand() % 100; });
+  generate(h_b.begin(), h_b.end(), []() { return rand() % 100; });
+
+  VecAddCUDA2(h_a.data(), h_b.data(), h_c.data(), n);
+
+  cudaDeviceSynchronize();
+
+  // Check result
+  verify_result_add(h_a, h_b, h_c, N);
+
+  cout << "COMPLETED SUCCESSFULLY\n";
 }
